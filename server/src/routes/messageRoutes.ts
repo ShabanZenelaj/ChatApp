3b48db9f4f351233659045afb4cff41e32fb9976
ShabanZenelaj <shabanzenelaj3@gmail.com>
2025-02-02 14:58:56 +0100

    Fix CORS, added forgotten middleware, reconfigured Docker

--- server/src/routes/messageRoutes.ts
+++ server/src/routes/messageRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import {checkDM} from "../controllers/messageControllers";
+import authenticate from "../middlewares/authenticate";
 
 const router = express.Router();
 
 // User Registration
-router.get("/dm", checkDM);
+router.get("/dm", authenticate, checkDM);
 
 export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -25,12 +25,12 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: ["http://localhost:5173", "http://localhost:4173"], credentials: true },
+    cors: { origin: ["http://localhost:5173", "http://localhost:4173", "http://localhost:80", "http://localhost"], credentials: true },
 });
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:4173"],
+    origin: ["http://localhost:5173", "http://localhost:4173", "http://localhost:80", "http://localhost"],
     credentials: true,
 }));
 app.use(helmet());
